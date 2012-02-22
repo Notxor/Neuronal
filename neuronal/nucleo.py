40866a49dc62ad1c28a6d8d2d3c0551d7624334b
Notxor <gnotxor@gmail.com>
2012-02-22 18:09:30 +0100

    Cambios menores sobre núcleo.

--- neuronal/nucleo.py
+++ neuronal/nucleo.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+#       Copyright (c) 2012 Notxor <gnotxor@gmail.com>
 #       Copyright (C) 2012 dddddd <dddddd@pyphiverses.org>
 
 #       This program is free software; you can redistribute it and/or modify
@@ -60,3 +61,27 @@ class Nucleo(Neurona):
         conveniente.
         """
         return self._neuronas
+
+    def proceso(self, modo='ciclo', veces=1):
+        """
+        Esto es un buen candidato para ser sustituido por un planificador. El
+        caso es que habría que tener mirar la utilización de threads. Pero los
+        threads en Python están controlados por el G.I.L. (Global Interpreter
+        Lock), cuyo acrónimo no es un diminutivo, que hace que sólo un hilo sea
+        ejecutado a la vez... así que no se adelanta mucho.
+        """
+        if (modo == 'ciclo'):
+            """
+            Supongo que una 'compreensión de lista' (list comprehension) será
+            más rápida que un bucle... siguiendo la programación funcional. Lo
+            que hace es recorrer todas las neuronas disparándolas.
+            """
+            for i in range(veces):
+                [n.intentar_disparo() for n in self._neuronas]
+        elif(modo == 'debug'):
+            """
+            El modo 'debug' aún no hace nada, pero lo que debe hacer es disparar
+            una a una las neuronas del núcleo para trazar el funcionamiento de
+            la red.
+            """
+            pass
