0f51754e8c90cad113995667ed7adbc48e4f49f5
Notxor <gnotxor@gmail.com>
2012-02-25 19:48:27 +0100

    Cambiando la licencia

--- tests/alltests.py
+++ tests/alltests.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-#       copyright (c) Notxor 2012
-#       Copyright (C) 2012 dddddd <dddddd@pyphiverses.org>
-
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
+# Neuronal - Framework for Neural Networks and Artificial Intelligence
 #
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
+# Copyright (C) 2012 dddddd <dddddd@pyphiverses.org>
+# Copyright (C) 2012 Notxor <gnotxor@gmail.com>
 #
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import sys
